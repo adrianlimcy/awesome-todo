8e0a3e44d1a808d8bdaadf6dcfe0f519263acaae
adrianlimcy <adrianlimcy@gmail.com>
2019-12-20 12:22:45 +0800

    Chapter 13 done

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,11 +1,42 @@
 <template>
-  <q-page class="flex flex-center">
-    <img alt="Quasar logo" src="~assets/quasar-logo-full.svg">
+  <q-page padding>
+    <!-- <input v-model="message" @keyup="handleKeyup"/> -->
+    <input v-model="message"
+      @keyup.esc="clearMessage"
+      @keyup.enter="alertMessage"
+      @mouseenter="alertMessage"
+    />
+    <button @click="clearMessage">Clear</button>
+    <h5>{{message}}</h5>
   </q-page>
 </template>
 
 <script>
-export default {
-  name: 'PageIndex'
-}
+  export default {
+    data() {
+      return {
+        message: "I love Vue.js so hard"
+      }
+    },
+    methods: {
+      clearMessage() {
+        this.message=''
+      },
+      // handleKeyup(e) {
+      //   console.log(e)
+      //   if (e.keyCode ==27) {
+      //     this.clearMessage()
+      //   }
+      //   else if (e.keyCode == 13) {
+      //     this.alertMessage()
+      //   }
+      // },
+      alertMessage() {
+        alert(this.message)
+      }
+    }
+  }
 </script>
+
+<style>
+</style>
