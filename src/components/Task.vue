dfd33177571b1e54ed2e7630ddcfc46f7f45b2fb
adrianlimcy <adrianlimcy@gmail.com>
2019-12-30 09:53:38 +0800

    Section 4 done

---
+++ src/components/Task.vue
@@ -0,0 +1,21 @@
+<template>
+  <!-- <li>
+    <div>{{task.name}} {{index}}</div>
+    <small>{{task.dueDate}} @ {{task.dueTime}}</small>
+    <button @click="deleteTask(index)">X</button>
+  </li> -->
+  <li>
+    <div><slot></slot> {{index}}</div>
+    <small>{{task.dueDate}} @ {{task.dueTime}}</small>
+    <button @click="deleteTask(index)">X</button>
+  </li>
+</template>
+
+<script>
+  export default {
+    props: ['task', 'index']
+  }
+</script>
+
+<style>
+</style>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,31 +1,44 @@
 <template>
 <q-page padding>
   <ul>
-    <li v-for="(task, index) in tasks">
+    <!-- <li v-for="(task, index) in tasks">
       <div>{{task.name}} {{index}}</div>
       <small>{{task.dueDate}} @ {{task.dueTime}}</small>
       <button @click="deleteTask(index)">X</button>
-    </li>
+    </li> -->
+    <!-- <task
+    v-for="(task, index) in tasks"
+    :task="task"
+    :index="index"></task> -->
+    <task
+    v-for="(task, index) in tasks"
+    :key="task.id"
+    :task="task"
+    :index="index">{{task.name}}</task>
   </ul>
 </q-page>
 </template>
 
 <script>
+  // import Task from 'components/Task.vue'
   export default {
     data() {
       return {
         tasks: [
           {
+            id: 1,
             name: 'Go to shop',
             dueDate: '2019/05/12',
             dueTime: '18:30'
           },
           {
+            id: 2,
             name: 'Get bananas',
             dueDate: '2019/05/13',
             dueTime: '14:30'
           },
           {
+            id: 3,
             name: 'Get apples',
             dueDate: '2019/05/14',
             dueTime: '16:00'
@@ -37,6 +50,10 @@
       deleteTask(index) {
         this.tasks.splice(index, 1)
       }
+    },
+    components: {
+      // 'task' : Task
+      'task' : require('components/Task.vue').default
     }
 
   }
