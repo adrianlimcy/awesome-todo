ff962759e244fd74b97a2ab535eb0d6a9dc70bf4
adrianlimcy <adrianlimcy@gmail.com>
2020-01-05 10:05:01 +0800

    Section 11 done

--- src/components/Tasks/Modals/AddTask.vue
+++ src/components/Tasks/Modals/AddTask.vue
@@ -1,67 +1,62 @@
 <template>
-  <q-card>
-    <modal-header>Add Task</modal-header>
-    <q-form @submit.prevent="submitForm">
-      <q-card-section>
-        <modal-task-name
-          :name.sync="taskToSubmit.name"
-          ref="modalTaskName"/>
-        <modal-due-date
-          :dueDate.sync="taskToSubmit.dueDate"
-          @clear="clearDueDate"/>
-        <modal-due-time
-          v-if = "taskToSubmit.dueDate"
-          :dueTime.sync="taskToSubmit.dueTime" />
-      </q-card-section>
+<q-card>
+  <modal-header>Add Task</modal-header>
+  <q-form @submit.prevent="submitForm">
+    <q-card-section>
+      <modal-task-name :name.sync="taskToSubmit.name" ref="modalTaskName" />
+      <modal-due-date :dueDate.sync="taskToSubmit.dueDate" @clear="clearDueDate" />
+      <modal-due-time v-if="taskToSubmit.dueDate" :dueTime.sync="taskToSubmit.dueTime" />
+    </q-card-section>
 
-      <modal-buttons />
+    <modal-buttons />
 
-      <!-- <pre>{{ taskToSubmit}}</pre> -->
-    </q-form>
-  </q-card>
-
+    <!-- <pre>{{ taskToSubmit}}</pre> -->
+  </q-form>
+</q-card>
 </template>
 
 <script>
-  import {mapActions} from 'vuex'
-  export default {
-    data() {
-      return {
-        taskToSubmit: {
-          name: '',
-          dueDate: '',
-          dueTime: '',
-          completed: false
-        }
+import {
+  mapActions
+} from 'vuex'
+export default {
+  data() {
+    return {
+      taskToSubmit: {
+        name: '',
+        dueDate: '',
+        dueTime: '',
+        completed: false
       }
-    },
-    methods: {
-      ...mapActions('tasks', ['addTask']),
-      submitForm() {
-        // console.log('submitForm');
-        this.$refs.modalTaskName.$refs.name.validate()
-        // console.log(this.$refs.name);
-        if (!this.$refs.modalTaskName.$refs.name.hasError) {
-          this.submitTask()
-        }
-      },
-      submitTask() {
-        // console.log('submitTask');
-        this.addTask(this.taskToSubmit)
-        this.$emit('close')
-      },
-      clearDueDate() {
-        this.taskToSubmit.dueDate=''
-        this.taskToSubmit.dueTime=''
-      }
-    },
-    components:{
-      'modal-header': require('components/Tasks/Modals/Shared/ModalHeader.vue').default,
-      'modal-task-name': require('components/Tasks/Modals/Shared/ModalTaskName.vue').default,
-      'modal-due-date': require('components/Tasks/Modals/Shared/ModalDueDate.vue').default,
-      'modal-due-time': require('components/Tasks/Modals/Shared/ModalDueTime.vue').default,
-      'modal-buttons': require('components/Tasks/Modals/Shared/ModalButtons.vue').default
     }
-
+  },
+  methods: {
+    ...mapActions('tasks', ['addTask']),
+    submitForm() {
+      // console.log('submitForm');
+      this.$refs.modalTaskName.$refs.name.validate()
+      // console.log(this.$refs.name);
+      if (!this.$refs.modalTaskName.$refs.name.hasError) {
+        this.submitTask()
+      }
+    },
+    submitTask() {
+      // console.log('submitTask');
+      this.addTask(this.taskToSubmit)
+      this.$emit('close')
+    },
+    clearDueDate() {
+      this.taskToSubmit.dueDate = ''
+      this.taskToSubmit.dueTime = ''
+    }
+  },
+  components: {
+    'modal-header': require('components/Tasks/Modals/Shared/ModalHeader.vue').default,
+    'modal-task-name': require('components/Tasks/Modals/Shared/ModalTaskName.vue').default,
+    'modal-due-date': require('components/Tasks/Modals/Shared/ModalDueDate.vue').default,
+    'modal-due-time': require('components/Tasks/Modals/Shared/ModalDueTime.vue').default,
+    'modal-buttons': require('components/Tasks/Modals/Shared/ModalButtons.vue').default
   }
+
+}
 </script>
--- src/pages/PageTodo.vue
+++ src/pages/PageTodo.vue
@@ -1,36 +1,13 @@
 <template>
 <q-page class="q-pa-md">
-  <q-list separator bordered v-if="Object.keys(tasks).length">
-    <task v-for="(task, key) in tasks"
-    :key="key"
-    :task="task"
-    :id="key">
-    </task>
-  </q-list>
-
-  <div class = "absolute-bottom text-center q-mb-lg">
-    <q-btn
-      @click="showAddTask = true"
-      round
-      color="primary"
-      size="24px"
-      icon="add" />
+  <no-tasks v-if="!Object.keys(tasksTodo).length"/>
+  <tasks-todo v-else :tasksTodo="tasksTodo" />
+  <tasks-completed :tasksCompleted="tasksCompleted" v-if="Object.keys(tasksCompleted).length"/>
+  <div class="absolute-bottom text-center q-mb-lg">
+    <q-btn @click="showAddTask = true" round color="primary" size="24px" icon="add" />
   </div>
   <q-dialog v-model="showAddTask">
-    <!-- <q-card>
-      <q-card-section>
-        <div class="text-h6">Alert</div>
-      </q-card-section>
-
-      <q-card-section>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum repellendus sit voluptate voluptas eveniet porro. Rerum blanditiis perferendis totam, ea at omnis vel numquam exercitationem aut, natus minima, porro labore.
-      </q-card-section>
-
-      <q-card-actions align="right">
-        <q-btn flat label="OK" color="primary" v-close-popup />
-      </q-card-actions>
-    </q-card> -->
-    <add-task @close="showAddTask=false"/>
+    <add-task @close="showAddTask=false" />
   </q-dialog>
 </q-page>
 </template>
@@ -45,42 +22,23 @@ export default {
       showAddTask: false
     }
   },
-  // data() {
-  //   return {
-  //     tasks: [
-  //       {
-  //         id: 1,
-  //         name: "Go to Shop",
-  //         completed: false,
-  //         dueDate: '2019/05/12',
-  //         dueTime: '18:30'
-  //       },
-  //       {
-  //         id: 2,
-  //         name: "Get bananas",
-  //         completed: false,
-  //         dueDate: '2019/05/13',
-  //         dueTime: '14:00'
-  //       },
-  //       {
-  //         id: 3,
-  //         name: "Get apples",
-  //         completed: false,
-  //         dueDate: '2019/05/14',
-  //         dueTime: '16:00'
-  //       }
-  //     ]
-  //   }
-  // }
   computed: {
     // tasks() {
     //   return this.$store.getters['tasks/tasks']
     // }
-    ...mapGetters('tasks', ['tasks'])
+    ...mapGetters('tasks', ['tasksTodo', 'tasksCompleted'])
+  },
+  mounted() {
+    this.$root.$on('showAddTask', () => {
+      this.showAddTask = true
+    })
   },
   components: {
-    'task' : require('components/Tasks/Task.vue').default,
-    'add-task' : require('components/Tasks/Modals/AddTask.vue').default
+    'task': require('components/Tasks/Task.vue').default,
+    'add-task': require('components/Tasks/Modals/AddTask.vue').default,
+    'tasks-todo': require('components/Tasks/TasksTodo.vue').default,
+    'tasks-completed': require('components/Tasks/tasksCompleted.vue').default,
+    'no-tasks': require('components/Tasks/NoTasks.vue').default
   }
 }
 </script>
