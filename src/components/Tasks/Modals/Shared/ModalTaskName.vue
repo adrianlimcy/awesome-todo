9877c8907b99d89328336c55bc5b089c8ab80d40
adrianlimcy <adrianlimcy@gmail.com>
2020-01-06 11:29:09 +0800

    99. Custom Directive done

--- src/components/Tasks/Modals/Shared/ModalTaskName.vue
+++ src/components/Tasks/Modals/Shared/ModalTaskName.vue
@@ -5,17 +5,30 @@
       outlined
       :value="name"
       @input="$emit('update:name', $event)"
+      :rules="[val => !!val || 'Field is required']"
+      autofocus
+      v-select-all
+      ref="name"
       label="Task name"
       class="col"
-      :rules="[val => !!val || 'Field is required']"
-      ref="name"
-      autofocus
       clearable />
   </div>
 </template>
 
 <script>
 export default {
-  props: ['name']
+  props: ['name'],
+  directives: {
+    selectAll: {
+      inserted(el) {
+        let input = el.querySelector('.q-field__native')
+        input.addEventListener('focus', () => {
+          if (input.value.length) {
+            input.select()
+          }
+        })
+      }
+    }
+  }
 }
 </script>
